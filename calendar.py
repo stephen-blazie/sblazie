de26feb300dbe144f98b7d53966eed00f9c48dcf
stephen-blazie <126737835+stephen-blazie@users.noreply.github.com>
2024-04-12 09:55:49 -0700

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#A simple Linux-based calendar utility for blind users (requires adgenda.py)
+#A full-featured Linux-based calendar utility for blind users with Grade2 braille support (requires adgenda.py)
 #Copyright Blazie Technologies LLC., All rights reserved
 #by Stephen Blazie
 
